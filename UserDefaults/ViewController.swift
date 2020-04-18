687ccfb8bda07f18074b6950d7b8cc23a07c4ebc
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-18 23:25:58 +0300

    Try out some code with UserDefaults

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let defaults = UserDefaults.standard
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseTouchID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        defaults.set("Dmitry Reshetnik", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dictionary = ["Name": "Dmitry", "Country": "UA"]
+        defaults.set(dictionary, forKey: "SavedDictionary")
+        
+        let extractedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let extractedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
     }
 
 
